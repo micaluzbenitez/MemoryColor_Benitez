75c5c1771f1418c1a15eaf2aef49054a8a1ca2d8
micaluzbenitez <benitezmicaelaluz@gmail.com>
2022-12-04 23:26:37 -0300

    Revert "Changed color comander start"
    This reverts commit df8b6cb0dec5c29790eca664174bb5933e6a2bcb.

--- MemoryColor_Benitez/Intermediate/Build/Win64/MemoryColor_Benitez/Shipping/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.34.31935
-#include "H:/AA Facu/Unreal/MemoryColor_Benitez/MemoryColor_Benitez/Intermediate/Build/Win64/MemoryColor_Benitez/Shipping/Engine/SharedPCH.Engine.ShadowErrors.h"
